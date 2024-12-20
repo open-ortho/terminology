4c5ad9f4ed960be0afda2f32571a8ad2f19c27f8
Toni Magni <email@antoniomagni.com>
2024-12-20 01:43:17 +0100

    Remove response_model from validation: FHIR validates already.

--- terminology/server/fhir_api.py
+++ terminology/server/fhir_api.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fhir.resources.valueset import ValueSet
 from terminology.resources.value_sets.dicom_scheduled_protocol import DicomScheduledProtocol
+import logging
 
 app = FastAPI()
 
-@app.get("/ValueSet/DicomScheduledProtocol", response_model=ValueSet)
+@app.get("/ValueSet/DicomScheduledProtocol")
 def get_dicom_scheduled_protocol():
     dsp = DicomScheduledProtocol()
+    logging.info(dsp.json(indent=2))  # Log the JSON representation of the resource
     return dsp.dict()
