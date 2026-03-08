2d953bb79a3e817ed4ad38772b327c4f64397d7d
Toni Magni <email@antoniomagni.com>
2026-03-08 22:31:24 +0100

    Add new record types for Photograph of Patient and Record of Cephalometric Tracing

--- terminology/resources/code_systems/cwru_ortho_image_types.py
+++ terminology/resources/code_systems/cwru_ortho_image_types.py
@@ -89,6 +89,11 @@ class CWRUOrthoRecordTypeCodeSystem(CodeSystem):  # Naming preserved for compati
                     definition="Scan of Occlusal Dental Plain Radiograph"
                 ),
                 CodeSystemConcept(
+                    code=f"{make_code('PH')}",
+                    display="Photograph of Patient",
+                    definition="Scan of Photograph of Patient"
+                ),
+                CodeSystemConcept(
                     code=f"{make_code('RE')}",
                     display="Record of Examination",
                     definition="Scan of Record of Examination"
@@ -97,6 +102,11 @@ class CWRUOrthoRecordTypeCodeSystem(CodeSystem):  # Naming preserved for compati
                     code=f"{make_code('RF')}",
                     display="Record of Facial and Jaw Examination",
                     definition="Scan of Paper Record of Facial and Jaw Examination"
+                ),
+                CodeSystemConcept(
+                    code=f"{make_code('RT')}",
+                    display="Record of Cephalometric Tracing",
+                    definition="Scan of Paper Record of Cephalometric Tracing"
                 ),
                 CodeSystemConcept(
                     code=f"{make_code('SM')}",
